cebe5fc6078f07ff9b06a914a712c5fede10a5f6
ingridpitta <ingridpitta056@gmail.com>
2019-11-24 09:44:06 -0300

    Nav's dropdown Menu

--- src/components/Nav/nav.component.jsx
+++ src/components/Nav/nav.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import { Link } from 'react-router-dom'
 import Logo from "../../assets/img/logo.svg";
 import MessageIcon from "../../assets/img/messageIcon.svg";
@@ -8,33 +8,100 @@ import OpenProfileIcon from "../../assets/img/openProfileIcon.svg";
 import SearchIcon from "../../assets/img/searchIcon.svg";
 import "./nav.styles.scss";
 
-const Nav = () => 
-<div className="nav">
-    <div className="nav--container">
-        <Link to='/'><img src={Logo} alt="Logo dataFortaleza"/></Link>
-        <div className="nav--status">
-            <input placeholder="Pesquisar"/>
-            <img className="nav--statusSearch" src={SearchIcon} alt="Pesquisar"/>
-            <div className="nav--statusMessage">
-                <Link to='/mensagens'><img src={MessageIcon} alt="Mensagens"/></Link>
-                <div className="nav--statusMessageStatus">2</div>
-            </div>
-            <div className="nav--statusNotification">
-                <Link to='/notificacoes'><img src={NotificationIcon} alt="Notificações"/></Link>
-                <div className="nav--statusNotificationStatus">2</div>
-            </div>
-            <div className="nav--profile">
-                <div className="nav--profilePicture">
-                    <img src={ProfilePicture} alt="Perfil"/>
-                    <div className="nav--profileStatus"></div>
+class Nav extends Component {
+    constructor(){
+        super();
+
+        this.state={
+            showMessages: false,
+            showNotifications: false,
+            showProfile: false
+        }
+    }
+
+
+    openProfile = () => {
+        this.setState({showProfile: !this.state.showProfile})
+    }
+
+    openMessages = () => {
+        this.setState({showMessages: !this.state.showMessages})
+    }
+
+    openNotifications = () => {
+        this.setState({showNotifications: !this.state.showNotifications})
+    }
+
+
+    render(){
+        const {showMessages , showNotifications, showProfile } = this.state
+        console.log(this.state)
+        return (
+            <div className="nav">
+                <div className="nav--container">
+                    <Link to='/'><img src={Logo} alt="Logo dataFortaleza"/></Link>
+                    <div className="nav--status">
+                        <input placeholder="Pesquisar"/>
+                        <img className="nav--statusSearch" src={SearchIcon} alt="Pesquisar"/>
+                            {showMessages ?
+                                <div onClick={this.openMessages} className="nav--statusMessage">
+                                    <img src={MessageIcon} alt="Mensagens"/>
+                                    <div className="nav--statusMessageStatus">2</div>
+                                    <div className="nav--Messages">
+
+                                    </div>
+                                </div>
+                                :
+                                <div onClick={this.openMessages} className="nav--statusMessage">
+                                    <img src={MessageIcon} alt="Mensagens"/>
+                                    <div className="nav--statusMessageStatus">2</div>
+                                </div>
+                            }
+
+                            {showNotifications ?
+                            <div onClick={this.openNotifications} className="nav--statusNotification">
+                                <img src={NotificationIcon} alt="Notificações"/>
+                                <div className="nav--statusNotificationStatus">2</div>
+                                <div className="nav--Notifications">
+
+                                </div>
+                            </div>
+                            :
+                            <div onClick={this.openNotifications} className="nav--statusNotification">
+                                <img src={NotificationIcon} alt="Notificações"/>
+                                <div className="nav--statusNotificationStatus">2</div>
+                            </div>
+                            }
+                        
+                        <div className="nav--profile">
+                            <div className="nav--profilePicture">
+                                <img src={ProfilePicture} alt="Perfil"/>
+                                <div className="nav--profileStatus"></div>
+                            </div>
+                            {showProfile ?
+
+                                <div>
+                                    <div className="nav--profileName">
+                                        <h2>Pedro Alencar</h2>
+                                        <img onClick={this.openProfile} src={OpenProfileIcon} alt="Ver Perfil"/>
+                                    </div>
+
+                                    <div onClick={this.openProfile} className="nav--profileOpen">
+                                    
+                                    </div>
+                                </div>
+                                :
+                                <div className="nav--profileName">
+                                    <h2>Pedro Alencar</h2>
+                                    <img onClick={this.openProfile} src={OpenProfileIcon} alt="Ver Perfil"/>
+                                </div>
+                            }
+                        </div>
+                    </div>
                 </div>
-                <div className="nav--profileName">
-                    <h2>Pedro Alencar</h2>
-                    <Link to='/perfil'><img src={OpenProfileIcon} alt="Ver Perfil"/></Link>
-                </div>
             </div>
-        </div>
-    </div>
-</div>;
+        )
+    }
+} 
 
 export default Nav;
