37365a271765f0c74a1f378a92c11b9fc8f22d7e
ingridpitta <ingridpitta056@gmail.com>
2019-12-10 17:50:33 -0300

    map component WIP

--- src/components/Charts/charts.component.jsx
+++ src/components/Charts/charts.component.jsx
@@ -23,12 +23,6 @@ class Charts extends Component {
       variable.properties.NOME,
       parseFloat(variable.properties.RENDA_M)
     ]);
-
-    // const newArray = [];
-
-    // newArray.push([[("number", "renda"), ("string", "bairro")]].concat(myData));
-
-    // return [newArray[0]];
     return myData;
   };
 
@@ -36,18 +30,17 @@ class Charts extends Component {
     const data = [[("string", "bairro"), ("number", "renda")]].concat(
       this.state.chartData
     );
-    console.log("test", data);
     return (
       <div className="charts">
         <Chart
-          width={"500px"}
-          height={"500px"}
+          width={"300px"}
+          height={"300px"}
           chartType="BarChart"
           loader={<div>Loading Chart</div>}
           data={data}
           options={{
             title: "Renda Média",
-            chartArea: { width: "500px" },
+            chartArea: { width: "300px" },
             isStacked: true,
             colors: ["#FF0058", "#FF0058"],
             hAxis: {
@@ -68,7 +61,3 @@ class Charts extends Component {
 }
 
 export default Charts;
-// data={[this.state.chartData]}
-//data={[["Task", "Hours per Day"], newData]}
-//data={[newData]}
-//myData.map(item => [parseFloat(item[0]), item[1]])
--- src/components/Map/map.component.jsx
+++ src/components/Map/map.component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import L from "leaflet";
 import { Map, Marker, Popup, TileLayer, GeoJSON } from "react-leaflet";
 import "./map.styles.scss";
 
@@ -13,20 +14,54 @@ class MapComponent extends Component {
     };
   }
 
-  // onEachFeature = (feature, layer) => {
-  //   layer.on({
-  //     mouseover:,
-  //     mouseout:,
-  //     click:
-  //   })
-  // }
+  highlightFeature = e => {
+    let layer = e.target;
+    layer.setStyle({
+      style: {
+        weight: 10,
+        stroke: true,
+        fill: true,
+        fillColor: "#000",
+        color: "#000",
+        fillOpacity: 1,
+        opacity: 1,
+        dashArray: ""
+      }
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+      layer.bringToFront();
+    }
+  };
+
+  resetHighlight = e => {
+    let geojson = L.geoJson(this.props.bairrosFortaleza);
+    geojson.resetStyle(e.target);
+  };
+
+  zoomToFeature = e => {
+    L.map.fitBounds(e.target.getBounds());
+  };
+
+  onEachFeature = (feature, layer) => {
+    layer.on({
+      mouseover: this.highlightFeature,
+      mouseout: this.resetHighlight
+      // click: this.zoomToFeature
+    });
+  };
 
   render() {
     const { bairrosFortaleza, rendaMedia } = this.props;
     const position = [this.state.lat, this.state.lng];
     return (
       <div className="mapid">
-        <Map center={position} zoom={this.state.zoom} scrollWheelZoom={false}>
+        <Map
+          center={position}
+          zoom={this.state.zoom}
+          scrollWheelZoom={false}
+          editing={true}
+        >
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -35,21 +70,27 @@ class MapComponent extends Component {
             data={bairrosFortaleza.features}
             id="bairros"
             style={() => ({
+              ref: "geojson",
+              stroke: true,
               color: "white",
               weight: 1,
-              fillColor: "none",
-              fillOpacity: 1,
+              fillColor: "#FF0058",
+              fillOpacity: 0.3,
+              dashArray: "3",
               className: "bairros"
             })}
+            onEachFeature={this.onEachFeature.bind(this)}
           />
           <GeoJSON
             data={rendaMedia.features}
             id="renda"
             style={() => ({
-              color: "white",
+              ref: "geojson",
+              stroke: true,
+              color: "transparent",
               weight: 1,
-              fillColor: "#FF0058",
-              fillOpacity: 0.5,
+              fillColor: "none",
+              fillOpacity: 0.2,
               className: "renda"
             })}
           />
@@ -63,5 +104,3 @@ class MapComponent extends Component {
 }
 
 export default MapComponent;
-
-//{ /*onEachFeature={this.onEachFeature.bind(this)}*/}
