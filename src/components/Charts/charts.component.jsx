e3d3d3b765a22f1a45e2940134f9316ae75bf73d
Eduardo Nunes <eduardo.nunes@globalhitss.com.br>
2019-12-12 07:33:35 -0300

    filtered ibge data

--- src/components/Charts/charts.component.jsx
+++ src/components/Charts/charts.component.jsx
@@ -1,63 +1,84 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Chart from "react-google-charts";
 import "./charts.styles.scss";
 
-class Charts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      chartData: null
+const Charts = (props) => {
+
+    const getRendaData = () => {
+        const rendaData = props.rendaMedia.features.map(variable => [
+            variable.properties.NOME,
+            parseFloat(variable.properties.RENDA_M)
+        ]);
+        return [[("string", "bairro"), ("number", "renda")]].concat(
+            rendaData
+        );
     };
-  }
 
-  componentDidMount() {
-    this.setDataToState();
-  }
+    const getIgbeData = () => {
+        console.log("layer selected", props.layerInfo);
+        const igbeData = props.ibge.features.map(variable => variable.properties);
+        const igbeFiltered = igbeData.filter((element) => {
+            return element.NM_BAIRRO.toString().normalize().toUpperCase() === props.layerInfo.name.toString().normalize().toUpperCase();
+        });
 
-  setDataToState = () => {
-    this.setState({ chartData: this.getData() });
-  };
+        console.log("ibge filtered", igbeFiltered);
+        return [[("string", "bairro"), ("number", "populacao")]].concat(
+            igbeFiltered.map(variable => [variable.NM_BAIRRO, variable.populacao])
+        );
+    };
 
-  getData = () => {
-    const myData = this.props.rendaMedia.features.map(variable => [
-      variable.properties.NOME,
-      parseFloat(variable.properties.RENDA_M)
-    ]);
-    return myData;
-  };
-
-  render() {
-    const data = [[("string", "bairro"), ("number", "renda")]].concat(
-      this.state.chartData
-    );
     return (
-      <div className="charts">
-        <Chart
-          width={"300px"}
-          height={"300px"}
-          chartType="BarChart"
-          loader={<div>Loading Chart</div>}
-          data={data}
-          options={{
-            title: "Renda Média",
-            chartArea: { width: "300px" },
-            isStacked: true,
-            colors: ["#FF0058", "#FF0058"],
-            hAxis: {
-              title: "renda",
-              minValue: 0,
-              maxValue: 3
-            },
-            vAxis: {
-              title: "bairro"
-            }
-          }}
-          // For tests
-          rootProps={{ "data-testid": "1" }}
-        />
-      </div>
-    );
-  }
-}
+        <div className="charts">
+            <Chart
+                width={"300px"}
+                height={"300px"}
+                chartType="BarChart"
+                loader={<div>Loading Chart</div>}
+                data={getRendaData()}
+                options={{
+                    title: "Renda Média",
+                    chartArea: {width: "300px"},
+                    isStacked: true,
+                    colors: ["#FF0058", "#FF0058"],
+                    hAxis: {
+                        title: "renda",
+                        minValue: 0,
+                        maxValue: 3
+                    },
+                    vAxis: {
+                        title: "bairro"
+                    }
+                }}
+                // For tests
+                rootProps={{"data-testid": "1"}}
+            />
+            <Chart
+                width={"300px"}
+                height={"300px"}
+                chartType="BarChart"
+                loader={<div>Loading Chart</div>}
+                data={getIgbeData()}
+                options={{
+                    title: "IGBE",
+                    chartArea: {width: "300px"},
+                    isStacked: true,
+                    colors: ["#FF0058", "#FF0058"],
+                    hAxis: {
+                        title: "renda",
+                        minValue: 0,
+                        maxValue: 3
+                    },
+                    vAxis: {
+                        title: "bairro"
+                    }
+                }}
+                // For tests
+                rootProps={{"data-testid": "1"}}
+            />
+            );
+        </div>
+    )
+};
+
 
 export default Charts;
